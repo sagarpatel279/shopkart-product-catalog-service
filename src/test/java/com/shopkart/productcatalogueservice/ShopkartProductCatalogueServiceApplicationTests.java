0743af722ed11249a62026fc807c589e277ea00c
sagarpatel279 <patelsagar913@gmail.com>
2025-04-17 20:44:59 +0530

    project initialized

---
+++ src/test/java/com/shopkart/productcatalogueservice/ShopkartProductCatalogueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopkart.productcatalogueservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopkartProductCatalogueServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
