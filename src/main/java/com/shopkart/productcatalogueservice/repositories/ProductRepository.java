c191bb9d2d5d45f667f1ee5bfddc84da13707b33
Sagar <patelsagar913@gmail.com>
2025-04-27 06:27:26 +0530

    minor changes

--- src/main/java/com/shopkart/productcatalogueservice/repositories/ProductRepository.java
+++ src/main/java/com/shopkart/productcatalogueservice/repositories/ProductRepository.java
@@ -4,8 +4,6 @@ import com.shopkart.productcatalogueservice.models.Product;
 import com.shopkart.productcatalogueservice.models.State;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
